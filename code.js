fc5ac583bb73fa375ce307fa6c4cc293602a1017
antonioii <asergiocosta.as@gmail.com>
2022-12-08 02:01:48 -0300

    Put a limit of two digits input
    Create a limit of only two digits per timer field using javascript.
    Also add some CSS to the timer input fields.

---
+++ code.js
@@ -0,0 +1,12 @@
+let numbers = document.querySelectorAll('.number');
+
+numbers.forEach(element => {
+    element.addEventListener('keypress', function(event) {
+        let value = element.value;
+        //when a key is pressed it'll check if there are more than two digits in number value
+        if (value.match(/\d{2,}/)) {
+            // If it does, prevent the change from being applied
+            event.preventDefault();
+          }
+    })
+});
