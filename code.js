49e9883f87c6d84b848f0f681880211b58c99273
antonio <asergiocosta.as@gmail.com>
2022-12-19 15:58:24 -0300

    Add js to turn playBtn in pauseBtn

--- code.js
+++ code.js
@@ -10,3 +10,36 @@ numbers.forEach(element => {
           }
     })
 });
+
+//Triggers the timerStart
+let buttonsBox = document.querySelector("#buttonsBox"); 
+let playBtn = document.getElementById("playBtn");
+playBtn.addEventListener("click", timerStart);
+numbers.forEach(element => {
+    element.addEventListener("keydown", function(event) {
+        if (event.keyCode === 13) {
+          timerStart();
+        }
+      });    
+});  
+
+function timerStart() {
+    //Substitue to a pause btn
+    buttonsBox.removeChild(playBtn);  
+    let pauseButton = document.createElement("button");
+    pauseButton.classList.add("pause-button");  
+        const pauseIcon = document.createElement("div");
+        pauseIcon.classList.add("pause-icon");  
+            const pauseBar1 = document.createElement("div");
+            pauseBar1.classList.add("pause-bar");
+            const pauseBar2 = document.createElement("div");
+            pauseBar2.classList.add("pause-bar");
+        pauseIcon.appendChild(pauseBar1);  
+        pauseIcon.appendChild(pauseBar2);  
+    pauseButton.appendChild(pauseIcon);
+    buttonsBox.insertBefore(pauseButton, buttonsBox.firstChild);
+    
+    //Starts the timer
+}
+
+
