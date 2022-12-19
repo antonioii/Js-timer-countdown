7450f529846216a9ae1318b06f957d3ac781c172
antonio <asergiocosta.as@gmail.com>
2022-12-19 16:17:13 -0300

    Add js to change pause back to play btn

--- code.js
+++ code.js
@@ -1,45 +1,64 @@
-//Setting how the numbers can be inputted
+//Setting how the numbers can be inputted:
 let numbers = document.querySelectorAll('.number');
 numbers.forEach(element => {
     element.addEventListener('keypress', function(event) {
         let value = element.value;
-        //when a key is pressed it'll check if there are more than two digits in number value
+        //when a key is pressed it'll check if there are more than two digits in number value:
         if (value.match(/\d{2,}/)) {
-            // If it does, prevent the change from being applied
+            // If it does, prevent the change from being applied:
             event.preventDefault();
           }
     })
 });
 
-//Triggers the timerStart
+//Play btn:
 let buttonsBox = document.querySelector("#buttonsBox"); 
 let playBtn = document.getElementById("playBtn");
-playBtn.addEventListener("click", timerStart);
-numbers.forEach(element => {
-    element.addEventListener("keydown", function(event) {
-        if (event.keyCode === 13) {
-          timerStart();
-        }
-      });    
-});  
+setPlayBtnListeners(playBtn);
 
+let pauseButton;
+let pauseIcon;
+let pauseBar1;
+let pauseBar2;
 function timerStart() {
-    //Substitue to a pause btn
+    //Substitue to a pause btn:
     buttonsBox.removeChild(playBtn);  
-    let pauseButton = document.createElement("button");
+    pauseButton = document.createElement("button");
     pauseButton.classList.add("pause-button");  
-        const pauseIcon = document.createElement("div");
+        pauseIcon = document.createElement("div");
         pauseIcon.classList.add("pause-icon");  
-            const pauseBar1 = document.createElement("div");
+            pauseBar1 = document.createElement("div");
             pauseBar1.classList.add("pause-bar");
-            const pauseBar2 = document.createElement("div");
+            pauseBar2 = document.createElement("div");
             pauseBar2.classList.add("pause-bar");
         pauseIcon.appendChild(pauseBar1);  
         pauseIcon.appendChild(pauseBar2);  
     pauseButton.appendChild(pauseIcon);
     buttonsBox.insertBefore(pauseButton, buttonsBox.firstChild);
+    pauseButton.addEventListener("click", resumePlay);
     
-    //Starts the timer
+    //Starts the timer:
 }
 
+//When pause button is triggered:
+function resumePlay(){
+    playBtn = document.createElement('button');
+    playBtn.id = 'playBtn';
+    buttonsBox.removeChild(pauseButton);
+    buttonsBox.insertBefore(playBtn, buttonsBox.firstChild);
+    setPlayBtnListeners(playBtn);
+}
+
+
+//Set the playBtn listeners
+function setPlayBtnListeners(playBtn) {
+    playBtn.addEventListener("click", timerStart);
+    numbers.forEach(element => {
+        element.addEventListener("keydown", function(event) {
+            if (event.keyCode === 13) {
+            timerStart();
+            }
+        });    
+    });
+}
 
